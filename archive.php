fc0d1803f493d4ef3a04d545e26f1367d61da864
James Andrews <thenetimp2000@yahoo.com>
2012-02-13 00:15:51 +0900

    copied index.php to archive.php to solve archive problem in the short term

--- archive.php
+++ archive.php
@@ -1,51 +1,31 @@
 <?php get_header(); ?>
-    <div id="main" class="container">
-      <div class="row">
-        <div id="blog" class="span9">
-          <?php if(have_posts()) : ?><?php while(have_posts()) : the_post();$post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-                 <?php if (is_category()): ?>
-                    <h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category:</h2>
-                  <?php elseif(is_tag()): ?>
-                    <h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
-                  <?php elseif (is_day()): ?>
-                    <h2>Archive for <?php the_time('F jS, Y'); ?>:</h2>
-                  <?php elseif (is_month()):  ?>
-                    <h2>Archive for <?php the_time('F, Y'); ?>:</h2>
-                  <?php elseif (is_year()): ?>
-                    <h2>Archive for <?php the_time('Y'); ?>:</h2>
-                  <?php elseif (is_author()): ?>
-                    <h2>Author Archive</h2>
-                  <?php elseif (isset($_GET['paged']) && !empty($_GET['paged'])): ?>
-                    <h2>Blog Archives</h2>
-                  <?php endif; ?>
-              <div class="post">
-              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-              <div class="entry">
-                <?php 
-                  the_post_thumbnail();
-                  the_excerpt();
-                ?>
-                <p class="postmetadata">
-                  <?php _e('Filed under&#58;'); ?>
-                  <?php the_category(' | '); ?>
-                  <?php _e('by'); ?>
-                  <?php the_author(); ?><br />
-                  <?php 
-                    comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;');
-                    edit_post_link('Edit Post', ' &#124; ', '');
-                  ?>
-                </p>
-              </div>
+    <div id="main" class="common-two-column content-container container row">
+      <div class="main-content span8">
+        <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+          <div class="post post-<?php the_ID(); ?>">
+            <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="post-author"><?php _e('by'); ?> <?php the_author_meta('first_name'); echo " "; the_author_meta('last_name'); ; ?></span></h3>
+            <div class="post-meta-data">
+              <?php the_time('F jS, Y') ?>
             </div>
-          <?php endwhile; ?>
-          <div class="navigation">
-          <?php posts_nav_link(); ?>
-          </div>          
-          <?php endif; ?>
-        </div>
-        <?php get_sidebar(); ?>
+            <div class="entry">
+              <?php 
+                the_post_thumbnail();
+                the_excerpt();
+              ?>
+            </div>
+          </div>
+          &nbsp;
+        <?php endwhile; ?>
+        <?php include_once('navigation_links.inc.php') ?>
+        <?php else: ?>
+          <p>There are currently no articles.</p>
+        <?php endif; ?>
       </div>
-    </div> <!-- /container -->
-<?php get_footer(); ?>
+      <div class="span3 sidebar-post sidebar">
+      <?php get_sidebar(); ?>
+      </div>
+    </div>
+<?php get_footer(); // include the footer ?>
+
 
 
