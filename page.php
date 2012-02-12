bfdaa0574524cc183639f0d090af21fb1e77a389
James Andrews <thenetimp2000@yahoo.com>
2012-02-13 01:19:12 +0900

    fixed call so that content is called properly

--- page.php
+++ page.php
@@ -7,18 +7,8 @@
             <div class="entry">
               <?php 
                 the_post_thumbnail();
-                the_excerpt();
+                the_content();
               ?>
-              <!-- <p class="postmetadata">
-                <?php _e('Filed under&#58;'); ?>
-                <?php the_category(' | '); ?>
-                <?php _e('by'); ?>
-                <?php the_author(); ?><br />
-                <?php 
-                  comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;');
-                  edit_post_link('Edit Post', ' &#124; ', '');
-                ?>
-              </p> -->
             </div>
           </div>
           &nbsp;
